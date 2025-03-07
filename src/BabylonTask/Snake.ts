032c0ab8de10a3b2a75c4bfd50a350198c5fe0db
wertengo <vitya.efimenko.05@mail.ru>
2025-03-07 11:39:08 +0300

    добавлен PointerDragBehavior на все кубики, в консоли выводится кубик который мы берем

--- src/BabylonTask/Snake.ts
+++ src/BabylonTask/Snake.ts
@@ -7,7 +7,9 @@ import {
     PhysicsJoint,
     PhysicsAggregate,
     PhysicsShapeType,
-    DistanceConstraint
+    DistanceConstraint,
+    PointerDragBehavior,
+    Quaternion
 } from "@babylonjs/core";
 
 export class Snake {
@@ -15,6 +17,7 @@ export class Snake {
     private snakeParts: Mesh[] = [];
     private physicsAggregates: PhysicsAggregate[] = [];
     private boxSize: number;
+    private meshCounter = 0;
 
     constructor(scene: Scene, boxSize = 0.5) {
         this.scene = scene;
@@ -23,14 +26,45 @@ export class Snake {
     }
 
     private createSnake() {
-        const distanceMultiplier = 3
         for (let i = 0; i < 4; i++) {
             const box = MeshBuilder.CreateBox(`box${i}`, { size: this.boxSize }, this.scene);
             // box.position.y = this.boxSize / 2;
             box.position.y = 4;
-            box.position.z = i * this.boxSize * 2 * distanceMultiplier;
-            box.metadata = i;
-            const boxAggregate = new PhysicsAggregate(box, PhysicsShapeType.BOX, { mass: 10 }, this.scene);
+            box.position.z = i * this.boxSize * 2;
+            box.metadata = { id: this.meshCounter++ }; 
+            console.log(`Created mesh with ID: ${box.metadata.id}`);
+
+            const dragBehavior = new PointerDragBehavior({});
+            dragBehavior.moveAttached = true;
+            dragBehavior.useObjectOrientationForDragging = false;
+
+            dragBehavior.onDragStartObservable.add(() => {
+                console.log(`Dragging started for box ${box.metadata.id}`);
+                // boxAggregate.body.disablePreStep = true;
+                boxAggregate.body.setMassProperties({ mass: 0.1 });
+            });
+
+            dragBehavior.onDragObservable.add((event) => {
+                console.log(`Dragging box ${box.metadata.id}`, event.delta);
+                box.position.addInPlace(event.delta);
+                const rotation = box.rotationQuaternion || Quaternion.Identity();
+                boxAggregate.body.setTargetTransform(box.position, rotation);
+            });
+
+            dragBehavior.onDragEndObservable.add(() => {
+                console.log(`Dragging ended for box ${box.metadata.id}`);
+                // boxAggregate.body.disablePreStep = false; 
+                boxAggregate.body.setMassProperties({ mass: 1 });
+
+                boxAggregate.body.setLinearVelocity(Vector3.Zero()); 
+                boxAggregate.body.setAngularVelocity(Vector3.Zero()); 
+                const rotation = box.rotationQuaternion || Quaternion.Identity();
+                boxAggregate.body.setTargetTransform(box.position, rotation);
+            });
+
+            box.addBehavior(dragBehavior);
+
+            const boxAggregate = new PhysicsAggregate(box, PhysicsShapeType.BOX, { mass: 1 }, this.scene);
     
             if(i > 0){
                 // const previousPart = this.snakeParts[i - 1];
@@ -52,9 +86,9 @@ export class Snake {
         }
     }
 
-    public moveSnake(direction: Vector3) {
-        this.snakeParts.forEach(part => {
-            part.position.addInPlace(direction);
-        });
-    }
+    // public moveSnake(direction: Vector3) {
+    //     this.snakeParts.forEach(part => {
+    //         part.position.addInPlace(direction);
+    //     });
+    // }
 }
